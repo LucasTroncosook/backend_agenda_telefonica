c632453cf929c9920027da4b3fe819933d378c7a
Lucas <LucasTroncoso176@gmail.com>
2024-11-23 19:41:35 -0300

    nuevas modificaciones

--- index.js
+++ index.js
@@ -12,31 +12,7 @@ morgan.token('body', (req) => JSON.stringify(req.body));
 
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'));
 
-let persons = [
-    { 
-      "id": 1,
-      "name": "Arto Hellas", 
-      "number": "040-123456"
-    },
-    { 
-      "id": 2,
-      "name": "Ada Lovelace", 
-      "number": "39-44-5323523"
-    },
-    { 
-      "id": 3,
-      "name": "Dan Abramov", 
-      "number": "12-43-234345"
-    },
-    { 
-      "id": 4,
-      "name": "Mary Poppendieck", 
-      "number": "39-23-6423122"
-    }
-]
-
 const date = new Date;
-const personsLength = persons.length;
 
 app.get('/api/persons', (request, response) => {
     Person.find({}).then(persons => {
